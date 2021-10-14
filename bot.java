29b5a53408db851186a1e2507c94d99d9014862c
schlingel <83114842+schlingelTV@users.noreply.github.com>
2021-10-14 20:59:03 +0200

    bot

---
+++ bot.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+public class bot {
+    public static void main(String[] args)throws IOException
+    {
+        Process p = Runtime.getRuntime().exec("cmd /c start cmd.exe");
+        //axaxaxaxaxaxaxaDay-Zaxaxaxaxaxaxaxa
+        //bitte funktionier
+        //penis☜(ﾟヮﾟ☜)☜(ﾟヮﾟ☜)☜(ﾟヮﾟ☜)( •_•)>⌐■-■;-):-((¬‿¬)(T_T)(T_T)(•_•);-)(¬‿¬):-((ˉ﹃ˉ)(•_•)
+        //gay
+
+
+    }
+}
